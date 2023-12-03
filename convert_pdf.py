46711017e421142f60e65b004d8b8b407c8e9ba5
Senechka <64082009+FooolyHARD@users.noreply.github.com>
2023-12-04 01:30:07 +0300

    added AA for folders

--- convert_pdf.py
+++ convert_pdf.py
@@ -1,8 +1,10 @@
 import argparse
+import os.path
 import re
 import subprocess
 from termcolor import colored
 from pdftolatex.pdf import *
+
 
 def convert(filepath):
     """Convert pdf at filepath to .tex file"""
@@ -12,30 +14,38 @@ def convert(filepath):
     if os.path.isdir(filepath):
         for f in os.listdir(filepath):
             convert(f)
-    
+
     filename = get_file_name(filepath)
     pdf = PDF(filepath)
     texfile = TexFile(pdf)
-    texfile.generate_tex_file(filename+".tex")
+    texfile.generate_tex_file(filename + ".tex")
+
 
 def parseregex(s, num):
-    regex = [r'\s[A-Z0-9]{4}\s', r'\d\d/\d\d/\d\d\d\d', r'\w{4}\s+\d+\s+\d+\s+\d+\.\d+\s+\d+\s\d+\s+\d+\.\d+\s+\d+\.\d+\s+\d+\.\d+', r'\d{2}\/\d{2}\/\d{4}'] #\w+\s\d+\s\d+\s\d+.\d+\s\d+\s\d+\s\d+.\d+\s\d+.\d+\s\d+.\d+.\s?\d+
-    #1 - Stations, 2 - 3.1AA, 3 - 3.2AA, 4 - 3.1BB, 5 - 3.2BB
+    regex = [r'\s[A-Z0-9]{4}\s', r'\d\d/\d\d/\d\d\d\d',
+             r'\w{4}\s+\d+\s+\d+\s+\d+\.\d+\s+\d+\s\d+\s+\d+\.\d+\s+\d+\.\d+\s+\d+\.\d+',
+             r'\d{2}\/\d{2}\/\d{4}']  # \w+\s\d+\s\d+\s\d+.\d+\s\d+\s\d+\s\d+.\d+\s\d+.\d+\s\d+.\d+.\s?\d+
+    # 1 - Stations, 2 - 3.1AA, 3 - 3.2AA, 4 - 3.1BB, 5 - 3.2BB
     substring_one = re.sub(r'[^A-Za-z0-9\s/\.\-]+', '', s)
     substring_two = re.sub(r'\s*\.\s*', '.', substring_one)
     substring_three = re.sub(r'O', '0', substring_two)
-    #print(substring_two)
+    # print(substring_two)
     matches = re.findall(regex[num], substring_three)
     matches = [match for match in matches if match != (" N0NE " or " NONE ")]
-    #print(matches)
+    # print(matches)
     return matches
 
-def clear(script):
-    subprocess.call(['sh', './'+script])
 
-def AAProcess(filename):
+def clear(script):
+    subprocess.call(['sh', './' + script])
+
+
+def AAProcess_forFile(filename, resname):
+    if not os.path.isfile(resname + '.txt'):
+        output = open(resname + '.txt', 'w')
+    else:
+        output = open(resname + '.txt', 'a')
     file = open(str(filename), 'r');
-    output = open('res_AA.txt', 'w')
     string = file.readlines()
     stations = parseregex(string[77], 0)
     output.write('User Stations:')
@@ -51,13 +61,45 @@ def AAProcess(filename):
     parsed_coordinates = []
     for i in range(0, len(stations)):
         parsed_coordinates.append(three_dot_two[i])
-    output.write(str(parsed_coordinates)+"\n")
+    output.write(str(parsed_coordinates) + "\n")
     output.write('Нужный формат\n')
     for i in range(0, len(stations)):
-        output.write(str(parsed_dates[i]) +' '+ str(parsed_coordinates[i]) + "\n")
+        output.write(str(parsed_dates[i]) + ' ' + str(parsed_coordinates[i]) + "\n")
     file.close()
 
-def BBProcess(filename):
+
+def AAProcess_forFolder(folder, res_name):
+    output = open(res_name + '.txt', 'a')
+    directory = folder
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            print(f)
+            convert(filename)
+            filename = str(filename).replace('.pdf', '')
+            AAProcess_forFile(filename + '.tex', res_name)
+            output.write("123")
+
+
+def BBProcess_forFile(filename):
+    file = open(str(filename), 'r');
+    output = open('res_BB.txt', 'a')
+    string = file.readlines()
+    three_dot_one = parseregex(string[129], 3)
+    output.write('3.1BB:')
+    parsed_dates = three_dot_one
+    output.write(str(parsed_dates) + '\n')
+    three_dot_two = parseregex(string[141], 2)
+    output.write('3.2BB:')
+    parsed_coordinates = three_dot_two
+    output.write(str(parsed_coordinates) + "\n")
+    output.write('Нужный формат\n')
+    for i in range(0, len(parsed_dates)):
+        output.write(str(parsed_dates[i]) + ' ' + str(parsed_coordinates[i]) + "\n")
+    file.close()
+
+
+def BBProcess_forFolder(filename):
     file = open(str(filename), 'r');
     output = open('res_BB.txt', 'w')
     string = file.readlines()
@@ -68,23 +110,26 @@ def BBProcess(filename):
     three_dot_two = parseregex(string[141], 2)
     output.write('3.2BB:')
     parsed_coordinates = three_dot_two
-    output.write(str(parsed_coordinates)+"\n")
+    output.write(str(parsed_coordinates) + "\n")
     output.write('Нужный формат\n')
     for i in range(0, len(parsed_dates)):
-        output.write(str(parsed_dates[i]) +' '+ str(parsed_coordinates[i]) + "\n")
+        output.write(str(parsed_dates[i]) + ' ' + str(parsed_coordinates[i]) + "\n")
     file.close()
+
+
 def main():
     clear('remove_locals.sh')
     parser = argparse.ArgumentParser(description="Generate a .tex file from a .pdf file.")
     parser.add_argument('--filepath', type=str, help="Path to pdf to be converted")
-    parser.add_argument('--folderpath', type=str, help="Path to folder containing pdfs to be converted. All pdfs in the folder will be converted")
-    
+    parser.add_argument('--folderpath', type=str,
+                        help="Path to folder containing pdfs to be converted. All pdfs in the folder will be converted")
+
     args = parser.parse_args()
 
     filepath = args.filepath
     folderpath = args.folderpath
     if folderpath:
-        convert(folderpath)
+        AAProcess_forFolder(folderpath, "res_AA_folder")
     else:
         while True:
             print("Выберите вариант AA или BB:")
@@ -92,13 +137,13 @@ def main():
             if choice == "AA":
                 filename = str(filepath).replace('.pdf', '')
                 convert(filepath)
-                AAProcess(filename+'.tex')
+                AAProcess_forFile(filename + '.tex')
                 print(colored("Досвидания!", 'green'))
                 break
             if choice == "BB":
                 filename = str(filepath).replace('.pdf', '')
                 convert(filepath)
-                AAProcess(filename + '.tex')
+                BBProcess_forFile(filename + '.tex')
                 print(colored("Досвидания!", 'green'))
                 break
             else:
